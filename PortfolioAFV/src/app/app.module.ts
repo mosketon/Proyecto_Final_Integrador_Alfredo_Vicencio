77bfb1bfdf93c38f97fa1c1e3934e3e2e2a4386f
mosketon <94483901+mosketon@users.noreply.github.com>
2022-03-31 21:04:27 -0300

    Inicio de los primeros componentes
    Creación de los componentes generales y componentes del header

--- PortfolioAFV/src/app/app.module.ts
+++ PortfolioAFV/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { RedesComponent } from './components/redes/redes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    LogoAPComponent,
+    RedesComponent
   ],
   imports: [
     BrowserModule
