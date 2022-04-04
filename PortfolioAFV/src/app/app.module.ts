0fd91bad6c057aa6d17ca8e208211cd46a154f3c
mosketon <94483901+mosketon@users.noreply.github.com>
2022-04-04 20:10:08 -0300

    Mejora alineación iconos
    Se modificó el código, agregando div Class="d-flex" para usar las propiedades de bootstrap

--- PortfolioAFV/src/app/app.module.ts
+++ PortfolioAFV/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BodyComponent } from './components/body/body.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { RedesComponent } from './components/redes/redes.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { RedesComponent } from './components/redes/redes.component';
     BodyComponent,
     FooterComponent,
     LogoAPComponent,
-    RedesComponent
+    RedesComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
