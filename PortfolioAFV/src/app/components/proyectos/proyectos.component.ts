c99f5cbe5dd5c7bfde54ae7f893ceb239f05bed3
mosketon <94483901+mosketon@users.noreply.github.com>
2022-04-10 20:28:38 -0300

    Modulos funcionando lectura del JSON. Falta pulir

--- PortfolioAFV/src/app/components/proyectos/proyectos.component.ts
+++ PortfolioAFV/src/app/components/proyectos/proyectos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-proyectos',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./proyectos.component.css']
 })
 export class ProyectosComponent implements OnInit {
-
-  constructor() { }
+proyectosList:any
+  constructor(private datosPortfolio: PortfolioService) { }
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe (data=>{
+      this.proyectosList=data.proyectos;
+    })
   }
 
 }
