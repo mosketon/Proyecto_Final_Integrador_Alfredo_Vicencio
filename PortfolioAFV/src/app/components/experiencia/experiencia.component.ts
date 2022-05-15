815cebd1b512aec6dce70d48e5547d2a8e5b7cd2
mosketon <94483901+mosketon@users.noreply.github.com>
2022-05-14 22:15:18 -0300

    Botones
    agregado de botones editar, agregar y borrar. mejoras en el diseño

--- PortfolioAFV/src/app/components/experiencia/experiencia.component.ts
+++ PortfolioAFV/src/app/components/experiencia/experiencia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-experiencia',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiencia.component.css']
 })
 export class ExperienciaComponent implements OnInit {
-
-  constructor() { }
+experienciaList: any
+  constructor(private datosportfolio:PortfolioService) { }
 
   ngOnInit(): void {
+    this.datosportfolio.obtenerDatos().subscribe (data=>{
+      this.experienciaList=data.experiencia;
+    })
   }
 
 }
